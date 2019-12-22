b4324323d4d1ad32fcd0122d3a99db1f03380d71
Otsdarva1 <yamazaki192748@gmail.com>
2019-12-22 19:15:16 +0900

    practice modules

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import _ from 'lodash'
+import { nijou, hoge } from './utilities'
+import Lion from './utilities'
+
+console.log(nijou(11))
+console.log(hoge)
+console.log(Lion.say())
 
 const component = () => {
     const element = document.createElement('div');

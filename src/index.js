1940fd8781f87027cc2f682e568340b00052027a
Otsdarva1 <yamazaki192748@gmail.com>
2020-01-06 23:23:01 +0900

    implement url-loader and file-loader

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import _ from 'lodash'
 import './style.css'
+import logo from './logo.png'
 
 const component = () => {
     const element = document.createElement('div');
-    const array = ['Hello', 'webpack', '!!']
-    element.innerHTML = _.join(array, ' ')
+    const array = ['Hello', 'webpack', '!!'];
+    element.innerHTML = _.join(array, ' ');
     return element;
 }
 
 document.body.appendChild(component());
-document.body.classList.add('haikei')
+document.body.classList.add('haikei');
+
+const image = new Image();
+image.src = logo;
+document.body.appendChild(image);
